655dacc4e3b8a45665bc8a7dae9bc7b49fd5ad54
danielsdev81 <danielsilverio81@utlook.com>
2024-04-23 18:12:40 -0300

    add: first migrate

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
+import express from "express";
 
-const app = express()
+const app = express();
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.json({hello: 'Hello, World'}));
+router.get("/", (req, res) => res.json({ hello: "Hello, World" }));
 app.use(router);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`App Start in PORT:${PORT}`);
-    console.log(`Access: http://localhost:3000/`);
-})
+  console.log(`App Start in PORT:${PORT}`);
+  console.log(`Access: http://localhost:3000/`);
+});
