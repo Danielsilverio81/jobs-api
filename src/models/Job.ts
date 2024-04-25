85a07fa55f2749ca9e442edf62eee205be866313
danielsdev81 <danielsilverio81@utlook.com>
2024-04-25 14:39:23 -0300

    add migrate, seed and model jobs

---
+++ src/models/Job.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from "sequelize"
+import { sequelize } from "../database"
+
+export interface JobInstance extends Model {
+    id: number
+    title: string
+    description: string
+    limitDate: Date
+    companyId: number
+}
+
+const job = sequelize.define<JobInstance>('jobs', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    limitDate: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    companyId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'companies',
+            key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT'
+    }
+})
+
+export {job};
